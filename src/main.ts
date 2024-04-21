725fa24b205dd5b1179c6d31bc8ca59bf6fd61fc
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-04-21 16:44:52 +0300

    chore:  tweaks
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { bytesToString, stringify } from 'npm:viem';
 
 import { blobscriptions } from './index.ts';
 
+const webhooksList = ['https://webhook.site/fef05c0e-9770-45c4-9284-50875f416f25'];
+
 /**
  * This file is the entry point for the indexing server, run on something like Fly.io servers.
  * It uses the `blobscriptions` function to handle incoming payloads from the blockchain,
  * and then sends them to the provided webhook URLs, or calls the provided functions.
  */
 
-blobscriptions(['https://webhook.site/fef05c0e-9770-45c4-9284-50875f416f25'], {
-  logging: true,
-}).catch((e) => {
+blobscriptions(webhooksList, { logging: true }).catch((e) => {
   console.error('Failure in blobscriptions handling...', e);
 });
 
